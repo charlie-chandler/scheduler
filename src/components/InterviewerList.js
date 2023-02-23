6e5d59f67fc06966a31530c7d523f511c4993a82
cchand-vanc <cmchandler@live.co.uk>
2023-02-22 18:42:42 -0800

    Refactored using props and state

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,29 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
-import "components/InterviewerListItem.scss";
+
+import "components/InterviewerList.scss";
 
 function InterviewerList(props) {
+
+  const parsedInterviewers = props.interviewers.map((interviewer) => {
+    return <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={() => {props.setInterviewer(interviewer.id)}}
+    />
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {parsedInterviewers}
+      </ul>
     </section>
+
   );
 }
+
+export default InterviewerList;
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,25 +1,24 @@
 import React, { useState } from "react";
 import classNames from "classnames";
-
 import "./InterviewerListItem.scss";
 
 
 export default function InterviewerListItem(props) {
 
-  const [interviewer, setInterviewer] = useState(props.id);
-
   const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected
   });
 
+  //
+
   return (
-    <li className={interviewerClass} onClick ={() => props.setInterviewer(props.id)}>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.name}
+      {props.selected && props.name}
     </li>
   );
 }
