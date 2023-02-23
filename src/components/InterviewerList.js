e96908ce7202b8113a5a910cc83644bf8dc62f32
cchand-vanc <cmchandler@live.co.uk>
2023-02-22 17:36:29 -0800

    Creates InterviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerListItem.scss";
+
+function InterviewerList(props) {
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"></ul>
+    </section>
+  );
+}
