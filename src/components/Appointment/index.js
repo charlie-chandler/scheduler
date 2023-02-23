2d2b7a926e7162f2267df043f77ea9ec16a99657
cchand-vanc <cmchandler@live.co.uk>
2023-02-23 11:22:12 -0800

    Adds export

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,2 +1,10 @@
-import Appointment from "components/Appointment";
+import React from "react";
+import "./styles.scss";
 
+function Appointment(props) {
+  return (
+    <article className="appointment"></article>
+  )
+}
+
+export default Appointment;
