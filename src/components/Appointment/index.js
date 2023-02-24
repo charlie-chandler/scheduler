aa64e89bcc888866b747f91811003f74c16ae6dd
cchand-vanc <cmchandler@live.co.uk>
2023-02-23 20:58:17 -0800

    Fixes interviewer.name bug

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React from 'react'
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 import "./styles.scss";
 
+
 function Appointment(props) {
+  console.log(props)
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header 
+        time={props.time}
+      />
+      {props.interview ?
+      <>
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      </>
+      :
+      <>
+        <Empty/>
+
+      </>
+      }
+    </article>
   )
 }
 
