f5f9864f9994b174d812cb2e25a994cb314c0008
cchand-vanc <cmchandler@live.co.uk>
2023-02-24 18:17:52 -0800

    Adds getAppointmentsForDay function and its tests

---
+++ src/helpers/selectors.js
@@ -0,0 +1,30 @@
+
+export function getAppointmentsForDay(state, day) {
+  let selectAppts = [];
+  let targetAppts = null;
+
+  //If days data is empty
+  if (state.days.length < 1) {
+    return [];
+  }
+
+  //Match the days
+  for (let element of state.days) {
+    if (element.name === day) {
+      targetAppts = element.appointments;
+    }
+  }
+
+  //If day not not found
+  if (!targetAppts) {
+    return [];
+  }
+
+  //Push appt info into empty array
+  for (let appt of targetAppts) {
+    selectAppts.push(state.appointments[appt]);
+  }
+
+  return selectAppts;
+
+};
