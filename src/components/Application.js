5872f4ebea34caa3e1fe1854e5e9ce10b49a3537
cchand-vanc <cmchandler@live.co.uk>
2023-02-25 10:32:56 -0800

    Imports getInterview function

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import getAppointmentsForDay from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 import "components/Application.scss";
 
@@ -12,35 +12,36 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
   
   const setDay = day => setState({ ...state, day });
-  //const setDays = days => setState({...state, days});
   
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   
-  //Renders appt data
-  // useEffect(() => {
-  //   axios.get("/api/days")
-  //   .then((response) => {
-  //     console.log('response', response);
-  //     setDays(response.data)
-  //   })
-  // }, []); 
-  
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
+    return <Appointment
+      key={appointment.id}
+      interview={interview}
+      {...appointment} // we can spread if we want every key in an object to become a prop for this component
+    />
+  });
+
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
+      axios.get('api/interviewers')
     ])
     .then((all) => {
-      console.log('all[0]', all[0])
-      console.log('all[1]', all[1])
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
     })
   }, []);
-
+  
+  
   return (
     <main className="layout">
       <section className="sidebar">
@@ -64,12 +65,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {dailyAppointments.map((appointment) => {
-          return <Appointment
-            key={appointment.id}
-            {...appointment} // we can spread if we want every key in an object to become a prop for this component
-          />
-        })}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
