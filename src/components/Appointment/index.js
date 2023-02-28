66973b3498cf28aa631388bd6e10d52c4167a819
cchand-vanc <cmchandler@live.co.uk>
2023-02-28 13:39:16 -0800

    Adds EDIT mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import useVisualMode from 'hooks/useVisualMode';
+
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Status from './Status';
-import useVisualMode from 'hooks/useVisualMode';
+import Confirm from './Confirm';
 
 import "./styles.scss";
 import Form from './Form';
@@ -14,10 +16,12 @@ const CREATE = "CREATE";
 const SAVE = "SAVE";
 const DELETE = "DELETE";
 const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
 
 
 function Appointment(props) {
 //console.log('props', props)
+
   const { mode, transition, back } = useVisualMode(
     props.interview && props.interview.interviewer ? SHOW : EMPTY
   );
@@ -26,7 +30,6 @@ function Appointment(props) {
     if (interviewer === null) {
       return;
     }
-    //console.log('props', props)
 
     const interview = {
       student: name,
@@ -35,11 +38,24 @@ function Appointment(props) {
     transition(SAVE);
     props.bookInterview(props.id, interview)
       .then(() => {transition(SHOW)})
-  }
+  };
 
-  function confirmDelete () {
+  function confirmDelete() {
       transition(CONFIRM)
-    }
+    };
+
+  function deleteAppointment() {
+      transition(DELETE)
+      props.cancelInterview(props.id)
+      .then(() => {
+        transition(EMPTY);
+      });
+  };
+  
+  function editAppointment() {
+      console.log('props.interview:', props.interview)
+    transition(EDIT);
+  }
 
   return (
     <article className="appointment">
@@ -53,6 +69,8 @@ function Appointment(props) {
           interviewer={props.interviewers.find(interviewer => {
           return interviewer.id === props.interview.interviewer}
           )}
+          onDelete={confirmDelete}
+          onEdit={editAppointment}
         />
       )}
 
@@ -70,8 +88,25 @@ function Appointment(props) {
         />
       )}
 
+      {mode === EDIT && (
+        <Form 
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          interviewers={props.interviewers}
+          onSave={save}
+          onCancel={() => back()}
+        />
+      )}
+
       {mode === SAVE && <Status message={"Saving"} />}
       {mode === DELETE && <Status message={"Deleting"} />}
+
+      {mode === CONFIRM && <Confirm 
+        message={"Are you sure you want to delete this appointment?"} 
+        onCancel={() => back()}
+        onConfirm={() => deleteAppointment(props.id)}
+        />
+      }
 
 
     </article>
