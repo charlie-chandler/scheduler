23f48a7d2ec3c90982bf71f7436d4962e01d1ca3
cchand-vanc <cmchandler@live.co.uk>
2023-03-01 18:57:53 -0800

    Fixes updated Spots bug

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -11,27 +11,28 @@ const [state, setState] = useState({
 });
 
 
-function spotsRemaining (state) {
+function spotsRemaining (appointments) {
   //Find current day
   const currentDay = state.days.find(day => day.name === state.day)
 
   //Pull out the appt IDs for that specific day
   const apptIdArray = currentDay.appointments;
-  const apptList = Object.values(state.appointments);
+ // const apptList = Object.values(state.appointments);
 
   //Find null appts from the week's appt list
-  const allNullAppts = apptList.filter(appt => appt.interview === null)
+  //const allNullAppts = apptList.filter(appt => appt.interview === null)
   
   //Filter all null appts to show only those on the current day
-  const todayNullAppts = allNullAppts.filter(appt => apptIdArray.includes(appt.id))
+  const todayNullAppts = apptIdArray.filter(appt => appointments[appt].interview === null)
 
   const spots = todayNullAppts.length;
 
   return spots;
 }
 
-function updateSpots () {
-  const spots = spotsRemaining(state);
+function updateSpots (appointments) {
+  const spots = spotsRemaining(appointments);
+  console.log('spots', spots)
   const currentDay = state.days.find(day => day.name === state.day)
 
   //Add spots value to currentDay object
@@ -43,6 +44,7 @@ function updateSpots () {
   updatedDays[currentDayIndex] = updatedCurrentDay;
 
    const updatedState = {...state, days: updatedDays}
+   console.log('updatedState', updatedState)
 
   return updatedState
 }
@@ -50,8 +52,6 @@ function updateSpots () {
 //Book Interview
 
 function bookInterview(id, interview) {
-  const updatedState = updateSpots();
-
   const appointment = {
     ...state.appointments[id],
     interview: { ...interview }
@@ -60,14 +60,15 @@ function bookInterview(id, interview) {
     ...state.appointments,
     [id]: appointment
   };
+  const updatedState = updateSpots(appointments);
 
   
   return axios.put(`/api/appointments/${id}`, {interview})
   .then((response) => {
     setState({
       ...state,
-      appointments,
-      updatedState
+      ...updatedState,
+      appointments
     });
     return response
   })
@@ -80,24 +81,25 @@ function bookInterview(id, interview) {
 //Cancel Interview
 
 function cancelInterview (id) {
-  const updatedState = updateSpots();
+  
+  const appointment = {
+    ...state.appointments[id],
+    interview: null
+  };
+  const appointments = {
+    ...state.appointments,
+    [id]: appointment
+  };
+  
+  const updatedState = updateSpots(appointments);
 
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    
     return axios
       .delete(`/api/appointments/${id}`)
       .then(() => {
         setState({
           ...state,
-          appointments,
-          updatedState
+          ...updatedState,
+          appointments
         });
       })
       .catch(err => {
